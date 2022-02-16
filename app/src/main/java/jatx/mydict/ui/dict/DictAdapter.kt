cf12ccd763f9d3f3789d6c56f4792adb4e79ec4c
Evgeny Tabatsky <e.tabatsky@gmail.com>
2022-02-16 15:13:50 +1000

    RecyclerView adapter replaced with ListAdapter

--- app/src/main/java/jatx/mydict/ui/dict/DictAdapter.kt
+++ app/src/main/java/jatx/mydict/ui/dict/DictAdapter.kt
@@ -3,20 +3,21 @@ package jatx.mydict.ui.dict
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import jatx.mydict.R
 import jatx.mydict.databinding.ItemDictBinding
 import jatx.mydict.domain.models.Word
 
-class DictAdapter: RecyclerView.Adapter<DictAdapter.VH>() {
+class DictAdapter: ListAdapter<DictAdapter.WordWithPosition, DictAdapter.VH>(WordDiffUtil()) {
 
-    private val words: ArrayList<Word> = arrayListOf()
     var onWordClick: (Word) -> Unit = {}
 
     fun updateWords(words: List<Word>) {
-        this.words.clear()
-        this.words.addAll(words)
-        notifyDataSetChanged()
+        submitList(words.mapIndexed { index, word ->
+            WordWithPosition(index, word)
+        })
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
@@ -26,19 +27,37 @@ class DictAdapter: RecyclerView.Adapter<DictAdapter.VH>() {
     }
 
     override fun onBindViewHolder(holder: VH, position: Int) {
-        val word = words[position]
-        with (holder.binding) {
-            tvOriginal.text = "${position + 1}. ${word.original}"
-            tvTranslation.text = word.translation
-        }
-        holder.v.setOnClickListener {
-            onWordClick(word)
+        val word = getItem(position)
+        holder.bindTo(position, word)
+    }
+
+    inner class VH(private val v: View): RecyclerView.ViewHolder(v) {
+        private val binding = ItemDictBinding.bind(v)
+
+        fun bindTo(position: Int, wordWithPosition: WordWithPosition) {
+            with (binding) {
+                with (wordWithPosition) {
+                    tvOriginal.text = "${position + 1}. ${word.original}"
+                    tvTranslation.text = word.translation
+                }
+            }
+            v.setOnClickListener {
+                onWordClick(wordWithPosition.word)
+            }
         }
     }
 
-    override fun getItemCount() = words.size
+    class WordDiffUtil: DiffUtil.ItemCallback<WordWithPosition>() {
+        override fun areItemsTheSame(oldItem: WordWithPosition, newItem: WordWithPosition) =
+            (oldItem == newItem)
 
-    class VH(val v: View): RecyclerView.ViewHolder(v) {
-        val binding = ItemDictBinding.bind(v)
+        override fun areContentsTheSame(oldItem: WordWithPosition, newItem: WordWithPosition) =
+            (oldItem == newItem)
+
     }
+
+    data class WordWithPosition(
+        val position: Int,
+        val word: Word
+    )
 }
